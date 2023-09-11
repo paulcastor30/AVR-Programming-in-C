502bef642c1a4e9eb6df71c48c41064cc903f5e5
Paul Rodolf Castor <80564346+paulcastor30@users.noreply.github.com>
2023-09-11 11:33:59 +0800

    Add files via upload

---
+++ Input and Output.c
@@ -0,0 +1,22 @@
+/*
+ * Arduino Mega 2560.c
+ *
+ * Created: 03/02/2023 11:16:55 am
+ * Author : Apple Rose
+ */ 
+
+#include <avr/io.h>
+#define F_CPU 16000000UL
+#include "util/delay.h"
+int main(void)
+{
+	DDRB = 0xFF;
+	DDRC = 0xFF;
+	while(1) //loop forever
+	{
+		PORTB ^= 0xFF; //toggle port B
+		PORTC ^= 0xFF; //toggle port C
+		_delay_ms(1000); //wait 1 second
+	}
+}
+
